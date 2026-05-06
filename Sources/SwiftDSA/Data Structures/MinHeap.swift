078c08d1106bac5f9f7d5227de06488edf2f5594
Ray <ray.moay@gmail.com>
2026-05-06 22:25:30 +0800

    minHeap asnwers

--- Sources/SwiftDSA/Data Structures/MinHeap.swift
+++ Sources/SwiftDSA/Data Structures/MinHeap.swift
@@ -16,80 +16,81 @@ class MinHeap {
     }
     
     func insert(value: Int) {
-        data.append(value)
-        heapifyUp(idx: length)
         length += 1
+        data.append(value) // adding to end is O(1)
+        heapifyUp(idx: length - 1)
     }
     
     func delete() -> Int? {
         guard length > 0 else { return nil }
         
-        let value = data[0]
+        data.swapAt(length - 1, 0)
+        let val = data.popLast()
         length -= 1
         
-        if length == 0 {
-            data = []
-            return value
-        }
-        
-        data.swapAt(0, length)
         heapifyDown(idx: 0)
-        return value
+        return val
     }
     
-    private func heapifyUp(idx: Int) {
-        if idx == 0 {
-            return
-        }
-        
-        let parentIdx = self.parent(idx: idx)
-        let parentValue = self.data[parentIdx]
-        let currValue = self.data[idx]
-        
-        if parentValue > currValue {
-            self.data.swapAt(parentIdx, idx)
-            heapifyUp(idx: parentIdx)
-        }
-    }
-    
-    private func heapifyDown(idx: Int) {
-        let leftIdx = self.leftChild(idx: idx)
-        let rightIdx = self.rightChild(idx: idx)
-        
-        if idx >= length || leftIdx >= length {
-            return
-        }
-        
-        if rightIdx >= length {
-            data.swapAt(leftIdx, idx)
-            heapifyUp(idx: leftIdx)
-            return
-        }
-        
-        let leftValue = self.data[leftIdx]
-        let rightValue = self.data[rightIdx]
-        let value = self.data[idx]
-        
-        if leftValue > rightValue && rightValue < value {
-            data.swapAt(rightIdx, idx)
-            heapifyDown(idx: rightIdx)
-        }
-        
-        if rightValue > leftValue && leftValue < value {
-            data.swapAt(leftIdx, idx)
-            heapifyDown(idx: leftIdx)
-        }
-    }
+    // private methods
     
     private func parent(idx: Int) -> Int {
         (idx - 1) / 2
     }
     
     private func leftChild(idx: Int) -> Int {
-        return 2 * idx + 1
+        2 * idx + 1
     }
     
     private func rightChild(idx: Int) -> Int {
-        return 2 * idx + 2
+        2 * idx + 2
+    }
+    
+    private func heapifyUp(idx: Int) {
+        // base condition
+        if idx == 0 { return }
+        
+        let parentIdx = parent(idx: idx)
+        let parentVal = data[parentIdx]
+        let val = data[idx]
+        
+        if parentVal < val { return }
+        
+        // swap and recurse
+        data.swapAt(parentIdx, idx)
+        heapifyUp(idx: parentIdx)
+    }
+    
+    private func heapifyDown(idx: Int) {
+        // base condition
+        if idx >= length { return }
+        
+        let leftIdx = leftChild(idx: idx)
+        let rightIdx = rightChild(idx: idx)
+        
+        // has child
+        if leftIdx >= length { return }
+        
+        let leftVal = data[leftIdx]
+        let val = data[idx]
+        
+        // only has left child
+        if rightIdx >= length {
+            if val > leftVal {
+                data.swapAt(leftIdx, idx)
+            }
+            return
+        }
+        
+        let rightVal = data[rightIdx]
+        
+        // recurse
+        if leftVal < rightVal && val > leftVal {
+            data.swapAt(leftIdx, idx)
+            heapifyDown(idx: leftIdx)
+        } else if rightVal < leftVal && val > rightVal {
+            data.swapAt(rightIdx, idx)
+            heapifyDown(idx: rightIdx)
+        }
     }
 }
