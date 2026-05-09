323b9e40251e8a34cd43f11c57ae1f940c8d8015
Ray <ray.moay@gmail.com>
2026-05-09 23:42:06 +0800

    multiple fibbonaci solutions

--- Sources/SwiftDSA/Problems/DynamicProgramming/Fibbonaci.swift
+++ Sources/SwiftDSA/Problems/DynamicProgramming/Fibbonaci.swift
@@ -7,30 +7,60 @@
 
 struct Fibbonaci {
     
-    /// [1,1,2,3,5,8,13...]
-    static func perform(_ n: Int) -> Int {
+    static func performTopDown(_ n: Int) -> Int {
         
-        var memo: [Int] = Array(repeating: -1, count: n + 1)
+        var memo: [Int] = Array(repeating: -1, count: n)
         
-        func getFib(_ n: Int) -> Int {
-            
-            if memo[n] != -1 {
-                return memo[n]
+        func getFib(i: Int) -> Int {
+            var result: Int = 0
+            if memo[i] != -1 {
+                return memo[i]
             }
             
-            var result = 0
-            // base case
-            if n == 0 {
-                result = 0
-            } else if n == 1 || n == 2 {
+            if i == 0 || i == 1 {
                 result = 1
             } else {
-                result = getFib(n - 1) + getFib(n - 2)
+                result = getFib(i: i - 1) + getFib(i: i - 2)
             }
-            memo[n] = result
+            memo[i] = result
             return result
         }
         
-        return getFib(n)
+        return getFib(i: n - 1)
+    }
+    
+    static func performBottomUpWithTabulation(_ n: Int) -> Int {
+        
+        var memo = Array(repeating: -1, count: n)
+        
+        for i in 1...n {
+            var result: Int = 0
+            if (i == 1 || i == 2) {
+                result = 1
+            } else {
+                result = memo[i-1-1] + memo[i-2-1]
+            }
+            memo[i-1] = result
+        }
+        return memo[n - 1]
+    }
+    
+    static func performNoMemory(_ n: Int) -> Int {
+        if n <= 2 { return 1 }
+        
+        var a = 0
+        var b = 1
+        for _ in 3...n {
+            let temp = b
+            b += a
+            a = temp
+        }
+        
+        return a + b
     }
 }
+
+/// 1. Recursive Backtracking
+/// 2. Top down DP using memoisation
+/// 3. Bottom-up DP, tabulation
+/// 4. Bottom-up, no memory, DP
--- Tests/SwiftDSATests/Problems/FibbonaciTests.swift
+++ Tests/SwiftDSATests/Problems/FibbonaciTests.swift
@@ -12,11 +12,29 @@ import Testing
 struct FibbonaciTests {
     
     @Test func assert() async throws {
-        #expect(Fibbonaci.perform(80) == 23416728348467685)
-        #expect(Fibbonaci.perform(5) == 5)
-        #expect(Fibbonaci.perform(4) == 3)
-        #expect(Fibbonaci.perform(3) == 2)
-        #expect(Fibbonaci.perform(2) == 1)
-        #expect(Fibbonaci.perform(1) == 1)
+        #expect(Fibbonaci.performTopDown(80) == 23416728348467685)
+        #expect(Fibbonaci.performTopDown(5) == 5)
+        #expect(Fibbonaci.performTopDown(4) == 3)
+        #expect(Fibbonaci.performTopDown(3) == 2)
+        #expect(Fibbonaci.performTopDown(2) == 1)
+        #expect(Fibbonaci.performTopDown(1) == 1)
+    }
+    
+    @Test func assertIterative() async throws {
+        #expect(Fibbonaci.performBottomUpWithTabulation(80) == 23416728348467685)
+        #expect(Fibbonaci.performBottomUpWithTabulation(5) == 5)
+        #expect(Fibbonaci.performBottomUpWithTabulation(4) == 3)
+        #expect(Fibbonaci.performBottomUpWithTabulation(3) == 2)
+        #expect(Fibbonaci.performBottomUpWithTabulation(2) == 1)
+        #expect(Fibbonaci.performBottomUpWithTabulation(1) == 1)
+    }
+    
+    @Test func assertNoMemory() async throws {
+        #expect(Fibbonaci.performNoMemory(80) == 23416728348467685)
+        #expect(Fibbonaci.performNoMemory(5) == 5)
+        #expect(Fibbonaci.performNoMemory(4) == 3)
+        #expect(Fibbonaci.performNoMemory(3) == 2)
+        #expect(Fibbonaci.performNoMemory(2) == 1)
+        #expect(Fibbonaci.performNoMemory(1) == 1)
     }
 }
